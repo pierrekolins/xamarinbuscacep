aa75069be7d1dbfc145008f344fe325186a17608
pierrekolins <pierrepneto@gmail.com>
2019-05-25 11:40:08 -0300

    Criadas as classes Endereco e ViaCepServico

---
+++ BuscaCEP/BuscaCEP/Servico/Modelo/Endereco.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuscaCEP.Servico.Modelo
+{
+    public class Endereco
+    {
+
+        /*
+         JSON
+URL: viacep.com.br/ws/01001000/json/ 
+UNICODE: viacep.com.br/ws/01001000/json/unicode/
+
+    {
+      "cep": "01001-000",
+      "logradouro": "Praça da Sé",
+      "complemento": "lado ímpar",
+      "bairro": "Sé",
+      "localidade": "São Paulo",
+      "uf": "SP",
+      "unidade": "",
+      "ibge": "3550308",
+      "gia": "1004"
+    }
+         */
+
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+        public string unidade { get; set; }
+        public string ibge { get; set; }
+        public string gia { get; set; }
+    }
+}
---
+++ BuscaCEP/BuscaCEP/Servico/ViaCepServico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using BuscaCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace BuscaCEP.Servico
+{
+    public class ViaCepServico
+    {
+
+        private static string URL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscaEnderecoViaCep(string cep) {
+            string NovaURL = string.Format(URL, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(NovaURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+        }
+    }
+}
